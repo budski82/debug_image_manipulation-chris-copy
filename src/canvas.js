e6a9433c06b5af5ddab71d3c893ab464020692ea
William Rutkowski <budski82@users.noreply.github.com>
2022-08-30 17:55:43 -0400

    removed gsap

--- src/canvas.js
+++ src/canvas.js
@@ -1,10 +1,10 @@
-import { gsap } from "./gsap";
+
 import {Dot} from './Dot.js'
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const img = document.querySelector('img');
-
+console.log(gsap);
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
