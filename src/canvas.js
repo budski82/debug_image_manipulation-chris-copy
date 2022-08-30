d9fab9cf51068c0e0e8bcfc6cbfd2946dcacb1ad
William Rutkowski <budski82@users.noreply.github.com>
2022-08-30 15:20:38 -0400

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -8,6 +8,7 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 ctx.drawImage(img, 0, 0);
+
 	const imageData = ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight).data;
   const dots = [];
   const pixels = [];
@@ -17,7 +18,7 @@ ctx.drawImage(img, 0, 0);
 
 	const x = (i /4) % img.naturalWidth;
 	const y = Math.floor(Math.floor(i / img.naturalWidth) / 4);
-
+ if(x % 5 === 0 && y % 5 === 0){
   pixels.push({
 		x,
 		y,
@@ -26,14 +27,18 @@ ctx.drawImage(img, 0, 0);
 		b: imageData[i + 2]
 	});
  };
+};
 
 pixels.forEach(pixel =>{
 	dots.push(new Dot(pixel.x, pixel.y, pixel.r, pixel.g, pixel.b, 0, 0));
 });
 
-ctx.clearRect(0, 0, innerWidth,innerHeight);
-dots.forEach(dot =>{
-	dot.draw(ctx);
-});
-
-console.log(dots);
+function animate() {
+	requestAnimationFrame(animate)
+	ctx.clearRect(0, 0, innerWidth,innerHeight);
+  dots.forEach(dot =>{
+		dot.draw(ctx);
+		dot.x++
+	});
+};
+animate();
